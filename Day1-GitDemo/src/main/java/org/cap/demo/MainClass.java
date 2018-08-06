53a3c2a00a5fce27ecb8fdf55894f9a987722de5
shafemoh <shafemoh@DIN73002996.corp.capgemini.com>
2018-08-06 10:54:39 +0530

    new commit

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,10 +3,14 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world!!!");
 		greet();
 	}
 public static void greet() {
-	System.out.println("Hello");
+	System.out.println("Hello!");
+	System.out.println("Hello1!");
+	System.out.println("Hello2!");
+	System.out.println("Hello3!");
+	
 }
 }
