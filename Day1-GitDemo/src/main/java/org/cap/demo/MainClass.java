38be8e5ca05bd23050e055f8fb2fcac0ef93684a
shafemoh <shafemoh@DIN73002996.corp.capgemini.com>
2018-08-06 12:31:42 +0530

    added sales

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,14 +3,17 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
+		SalesClass sales=new SalesClass();
 		System.out.println("Hello world!!!");
 		greet();
+		sales.show();
 	}
 public static void greet() {
 	System.out.println("Hello!");
 	System.out.println("Hello1!");
 	System.out.println("Hello2!");
 	System.out.println("Hello3!");
+
 	
 }
 }
